74ec2f713e96196bb70c1e88b9c56b81df3ca865
Lucas Torri <lucastorri@gmail.com>
2010-11-04 22:22:53 -0200

    selecting the correct type of worker manager.

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./zmq_jobs.rb manager listen_everyone 1 &
+#./zmq_jobs.rb manager listen_everyone 1 &
 
-./zmq_jobs.rb employee zmq1 1 &
-./zmq_jobs.rb employee zmq2 1 &
+#./zmq_jobs.rb employee zmq1 1 &
+#./zmq_jobs.rb employee zmq2 1 &
 
-./zmq_jobs.rb worker_manager listen_everyone 1 &
+./zmq_jobs.rb worker_manager listen1 1 &
 
 ./zmq_jobs.rb poor_worker number1 3 &
 ./zmq_jobs.rb poor_worker number1 3 &
